cd935c7bb300f077358777192a200c651550df24
CodyCTops <codytops1@gmail.com>
2022-07-09 22:40:41 -0230

    First Java Uploads

---
+++ Java-Main/src/Input.java
@@ -0,0 +1,20 @@
+// testing the import utility
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt user
+        System.out.println("Enter Username: ");
+
+        //read user input
+        String userName = scanner.nextLine();
+
+        //output user input
+        System.out.println("Username is: " + userName);
+    }
+}
---
+++ Java-Main/src/UserMultiplication.java
@@ -0,0 +1,27 @@
+//testing multiple user integer inputs
+
+import java.util.Scanner;
+
+public class UserMultiplication {
+    public static void main(String[] args) {
+
+        //Create Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //User Prompt
+        System.out.println("Please enter two integers for multiplication\n");
+
+        //User Input
+        System.out.println("Enter the first number: ");
+        int numOne = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int numTwo = scanner.nextInt();
+
+        //System Multiplication
+        int numThree = numOne * numTwo;
+
+        //System Output
+        System.out.println("\nThis equals: " + numThree);
+    }
+}
